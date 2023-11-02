0ce055b93c3aa5093597e1d7d75fa5519b5f6276
SamClercky <SamClercky@users.noreply.github.com>
2023-11-02 23:18:24 +0100

    Optimizations + statistics added

--- project-compgeom/src/main/java/be/ulbvub/compgeom/MainScreen.java
+++ project-compgeom/src/main/java/be/ulbvub/compgeom/MainScreen.java
@@ -11,6 +11,8 @@ public class MainScreen extends PApplet {
     private MouseClickEvent mouseClicked = null;
     private Button btnSave;
     private Button btnOpen;
+    private Button btnDecompose;
+    private Button btnExplain;
 
     public MainScreen() {
         super();
@@ -25,17 +27,33 @@ public class MainScreen extends PApplet {
     @Override
     public void setup() {
         polygonRegion = new PointDrawRegion();
-//        btnSave = new Button("Save", new PVector(10, 10), new PVector(50, 20));
+        polygonRegion.setListener((newPoint, polygon) -> {
+            if (polygon.points().size() > 3) {
+                dcel = TriangleDecomposition.triangulateYMonotonePolygon(polygonRegion.getPolygon());
+            }
+        });
+
         btnSave = new Button("Save", new PVector(0, 0), new PVector(50, 20));
         btnSave.setListener((evt) -> {
             System.out.println("Save file...");
             return null;
         });
 
-//        btnOpen = new Button("Open", new PVector(70, 10), new PVector(50, 20));
         btnOpen = new Button("Open", new PVector(0, 0), new PVector(50, 20));
         btnOpen.setListener((evt) -> {
             System.out.println("Open file...");
+            return null;
+        });
+
+        btnDecompose = new Button("Decompose", new PVector(0, 0), new PVector(90, 20));
+        btnDecompose.setListener((evt) -> {
+            System.out.println("Decomposing polygon");
+            return null;
+        });
+
+        btnExplain = new Button("Explain", new PVector(0, 0), new PVector(60, 20));
+        btnExplain.setListener((evt) -> {
+            System.out.println("Explain decomposition algorithm (tutorial part)");
             return null;
         });
     }
@@ -51,25 +69,30 @@ public class MainScreen extends PApplet {
                 .draw((ctx) -> {
                     ctx.fill(color(240));
                     VBox.with(ctx)
-                            .draw((ctx1) -> {
-                                btnSave.draw(ctx1);
-                            })
-                            .draw((ctx2) -> {
-                                btnOpen.draw(ctx2);
-                            });
+                            .draw((ctx1) -> btnSave.draw(ctx1))
+                            .draw((ctx2) -> btnOpen.draw(ctx2))
+                            .draw((ctx3) -> btnDecompose.draw(ctx3))
+                            .draw((ctx4) -> btnExplain.draw(ctx4));
                 })
                 .draw((ctx) -> {
                     ctx.fill(color(255));
                     StackBox.with(ctx)
                             .draw((ctx1) -> {
                                 polygonRegion.draw(ctx);
-                                int n = polygonRegion.getPolygon().points().size();
-                                if (n > 3 && (dcel == null || dcel.getVertices().size() != n)) {
-                                    dcel = TriangleDecomposition.triangulateYMonotonePolygon(polygonRegion.getPolygon());
-
-                                }
                                 if (dcel != null)
                                     dcel.draw(ctx);
+                            }).draw((ctx2) -> {
+                                // Draw statistics
+                                final var applet = ctx2.applet();
+
+                                final var pos = ctx2.size().copy().sub(new PVector(260, 50));
+
+                                ctx2.applyTextStyle();
+                                applet.text("Number of vertices: " + polygonRegion.getPolygon().points().size() + "\n" +
+                                                "Number of faces in decomposition: " + (dcel != null ? dcel.getFaces().size() : 0) + "\n" +
+                                                "Number of vertices in decomposition: " + (dcel != null ? dcel.getVertices().size() : 0) + "\n" +
+                                                "Number of edges in decomposition: " + (dcel != null ? dcel.getEdges().size() : 0),
+                                        pos.x, pos.y);
                             });
                 });
     }
--- project-compgeom/src/main/java/be/ulbvub/compgeom/PointDrawRegion.java
+++ project-compgeom/src/main/java/be/ulbvub/compgeom/PointDrawRegion.java
@@ -1,13 +1,21 @@
 package be.ulbvub.compgeom;
 
+import processing.core.PVector;
+
 import java.util.ArrayList;
+import java.util.function.BiConsumer;
 
 public class PointDrawRegion implements Drawable {
 
     private final Polygon polygon;
+    private BiConsumer<PVector, Polygon> listener;
 
     public PointDrawRegion() {
         polygon = new Polygon(new ArrayList<>());
+    }
+
+    public void setListener(BiConsumer<PVector, Polygon> listener) {
+        this.listener = listener;
     }
 
     @Override
@@ -15,6 +23,7 @@ public class PointDrawRegion implements Drawable {
         if (context.mouseClicked() != null) {
             final var point = context.region().toLocalPoint(context.mouseClicked().position());
             polygon.points().add(point);
+            listener.accept(point, polygon);
         }
         polygon.draw(context);
     }
